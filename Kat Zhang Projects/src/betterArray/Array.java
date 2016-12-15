33508585cf4e4b1ffd8d2ef06113c7ee117df5db
kazhang <kazhang@192.168.109.79>
2016-12-15 00:05:02 -0500

    Finished betterArray project

--- Kat Zhang Projects/src/betterArray/Array.java
+++ Kat Zhang Projects/src/betterArray/Array.java
@@ -2,67 +2,97 @@ package betterArray;
 
 //Katherine Zhang 
 //Array Remix 
+//CONTEXT: A concert hall wants to keep track of the amount of people attending concerts 
+//in a week for statistical purposes. They are interested in learning patterns to bring more
+//people into their venue. 
+
 public class Array {
-	private int[] array = new int[2];
-	int length = 0;
+	// defining array variable
+	int[] array;
 
+	// constructors
+	// Takes no arguments and initializes an empty array of size zero
 	Array() {
-		int a[] = new int[0];
+		array = new int[0];
 	}
 
-	Array(int[] val) {
+	// Takes an array of integers as the arg
+	Array(int[] n) {
+		array = n;
 	}
 
-	Array(int y) {
-		int b[] = new int[y];
+	// Takes an int and creates an array of that size with all values set to
+	// zero
+	Array(int x) {
+		array = new int[x];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = 0; // make each index 0
+		}
 	}
 
-	// returns the value at index i in the array
-	int getValue(int index) {
+	// returns the value at "index" in the array
+	// get() method tailored to Concert Population context
+	int getConcertPopulation(int index) {
 		return array[index];
 	}
 
 	// adds a number at the end of the array and increases its size by 1
 	void push(int val) {
-		int[] temp = new int[array.length + 1];
+		int[] temp = new int[array.length + 1]; // temporary place holder
 		for (int i = 0; i < array.length; i++) {
 			temp[i] = array[i];
 		}
 		temp[array.length] = val;
-		// the next statement is not really an assignment it is having array
+		// the next statement is not really an assignment, it is having array
 		// point to a different memory location
 		array = temp;
 	}
 
 	// returns the last item from the array and decreases the size of the array
 	int pop() {
-		int[] temp = new int[array.length - 1];
+		int[] temp = new int[array.length - 1]; // temporary place holder
 		for (int i = 0; i < array.length - 1; i++) {
 			temp[i] = array[i];
 		}
-		int lastValue = array[array.length - 1];
+		// returning last value in array
+		int lastValue = array[array.length - 1];// array.length - 1 bc its the
+												// last term
 		array = temp;
 		return lastValue;
 	}
 
 	// returns a string with every value in the array, e.g. “1, 2, 3, 4, 5”
 	public String toString() {
-		String string = "";
-		for (int i = 0; i < array.length; i++) 
-		{
-			string += array[i] + " ";
+		String string = "[";
+		// runs a loop through the array to place all values in the string
+		for (int i = 0; i < array.length; i++) {
+			string += array[i];// takes values and add a space between
+								// them
+			if (i != array.length - 1) {
+				string += ", " + "";
+			}
 		}
-		string += "";
-		return string;
+		string += "]";
+		return string; // returns the array
 	}
 
-	// adds number into the array + increases its size by 1
-	void insert(int index, int val) {
-		int[] temp = new int[array.length + 1];
-		for (int i = 0; i < array.length; i++) {
+	// takes arg index + value, adds val into the array @ index + increases its
+	// size by 1
+	void insert(int val, int index) {
+		int[] temp = new int[array.length + 1]; // creating a temporary array
+												// that has an increased size
+		for (int i = 0; i < index; i++) { // loops through the int of the array
+											// to fill in the temp array
 			temp[i] = array[i];
 		}
-		temp[index] = val;
+		temp[index] = val;// overrides the value at temp[index] with the value
+							// argument
+		for (int i = index; i < array.length; i++) {// loops through the temp
+													// array to fill the array
+													// array
+			temp[i + 1] = array[i];
+
+		}
 		array = temp;
 	}
 
@@ -76,7 +106,9 @@ public class Array {
 		for (int i = 0; i < array.length - 1; i++) {
 			temp[i] = array[i];
 		}
-		int lastValue = array[array.length - 1];
+		int lastValue = array[array.length - 1];// index starts @ 0, so its the
+												// array.length - 1 to account
+												// for the 0
 		array = temp;
 
 	}
@@ -93,15 +125,18 @@ public class Array {
 	}
 
 	// returns array
-	 int [] getArray() {
+	int[] getArray() {
 		return array;
 	}
 
 	// swaps the values in positions 'index1' and 'index2' in the array
 	void swap(int index1, int index2) {
-		int temp = array[index1];
-		array[index1] = array[index2];
-		array[index2] = temp;
+		int temp = array[index1]; // created a temporary variable to store first
+									// index
+		array[index1] = array[index2]; // swapped the int in first index for the
+										// int in second index
+		array[index2] = temp; // made int in 2nd index into temp variable, which
+								// holds the previous int in first index
 	}
 
 	// sorts the values in the array from least to greatest
@@ -110,9 +145,11 @@ public class Array {
 		int temp = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 1; j < (n - i); j++) {
-				if (array[j - 1] > array[j]) {
+				if (array[j - 1] > array[j]) { // if the first term is larger
+												// than the last term, then the
+												// temp holds the previous term
 					temp = array[j - 1];
-					array[j - 1] = array[j];
+					array[j - 1] = array[j];// swaps places within the array
 					array[j] = temp;
 				}
 
@@ -122,6 +159,7 @@ public class Array {
 
 	// returns the minimum value in the array
 	int min() {
+		// added my sort method in the beginning to sort our array
 		int n = array.length;
 		int temp = 0;
 		for (int i = 0; i < n; i++) {
@@ -133,12 +171,15 @@ public class Array {
 				}
 			}
 		}
+		// finds the first term in the array --> first term should be min if
+		// sorted
 		int x = array[0];
 		return x;
 	}
 
 	// returns the maximum value in the array
 	int max() {
+		// added my sort method in the beginning to sort out array
 		int n = array.length;
 		int temp = 0;
 		for (int i = 0; i < n; i++) {
@@ -151,12 +192,14 @@ public class Array {
 				}
 			}
 		}
+		// finds the last term in the array --> if array is sorted then the last
+		// term should be max
 		int x = array[array.length - 1];
 		return x;
 	}
 
 	// returns the average of the array
-	double average() {
+	double average() { // used double b/c I want decimal places
 		int sum = 0;
 		for (int i = 0; i < array.length; i++) {
 			sum = sum + array[i];
--- Kat Zhang Projects/src/betterArray/ArrayTest.java
+++ Kat Zhang Projects/src/betterArray/ArrayTest.java
@@ -1,22 +1,88 @@
 package betterArray;
 //Katherine Zhang 
+
 //Array Remix 
 
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		Array a = new Array(2);
 
-		//testing toString method
-		System.out.println(a.toString());
-		//testhing insert method
-		a.insert(0, 2);
-		
-		System.out.println(a.toString());
+		int[] concertPopulation = new int[7];
+		concertPopulation[0] = 10000; // Sunday
+		concertPopulation[1] = 14120; // Monday
+		concertPopulation[2] = 10411; // Tuesday
+		concertPopulation[3] = 9000; // Wednesday
+		concertPopulation[4] = 10131; // Thursday
+		concertPopulation[5] = 9801; // Friday
+		concertPopulation[6] = 7649; // Saturday
 
+		Array a = new Array(concertPopulation);
+		Array b = new Array(5);
+		Array c = new Array();
+
+		// Testing toString()
+		System.out.println(a.toString());
+		System.out.println(b.toString());
+		System.out.println(c.toString());
+
+		// Testing getValue()
+		System.out.println("Number of people at the hall on Tuesday: " + a.getConcertPopulation(2));
+		// Testing push()
+		// Relation to context: one worker added information from the beginning
+		// of one week
+		// to the end of another week by accident.
+		a.push(10421);
+		System.out.println("Push 10421: " + a);
+		// Testing pop()
+		// Relation to context: the worker removes the added information but
+		// keeps the added value
+		// so that they can add it to the next week's data
+		a.pop();
+		System.out.println("Pop 10421: " + a);
+		// Testing remove()
+		// Relation to context: the same worker realizes that they added the
+		// wrong data for Saturday,
+		// so they decide to take it out of the array
+		a.remove(6);
+		System.out.println("Removed Saturday from Array: " + a);
+		// Testing insert()
+		// Relation to context: the worker adds the correct data back into the
+		// array
+		a.insert(10000, 6);
+		System.out.println("Inserted new data for Saturday: " + a);
+		// Testing size()
+		// Relation to context: the worker forgot how many days were in a week,
+		// so he wants to know by using this method
+		System.out.println("Size: " + a.size());
+		// Testing getArray()
+		// Relation to context: worker just wants to see the array
+		a.getArray();
+		System.out.println("Returned array: " + a);
+		// Testing update()
+		// Relation to context: worker forgot that he was also their attending
+		// the concert
+		a.update(0, 10001);
+		System.out.println("Updated Monday: " + a);
+		// Testing swap ()
+		// Relation to context: worker put tuesday and wednesday data in the
+		// wrong place
+		a.swap(2, 3);
+		System.out.println("Swapped Tuesday and Wednesday: " + a);
+		// Testing sort()
 		a.sort();
-		System.out.println(a.toString());
-	
-		
-}
+		System.out.println("Sorted Array: " + a);
+		// Testing min()
+		// Relation to context: worker is curious about which day was the least
+		// popular
+		System.out.println("Min: " + a.min());
+		// Testing max()
+		// Relation to context: worker is curious about which day was the most
+		// popular
+		System.out.println("Max: " + a.max());
+		// Testing average()
+		// Relation to context: worker wants to see the average of people in
+		// this week
+		System.out.println("Average: " + a.average());
+
+	}
 }
