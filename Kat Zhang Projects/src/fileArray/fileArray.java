03ab5079c787f8fbaafd26036d86f2f11f9840e3
kazhang <kazhang@192.168.109.146>
2017-01-11 09:14:16 -0500

    fileArray + fileArrayTest

---
+++ Kat Zhang Projects/src/fileArray/fileArray.java
@@ -0,0 +1,16 @@
+package fileArray;
+
+public class fileArray {
+
+	public static void main(String[] args) {
+		int[] array;
+	}
+	fileArray(){
+	}
+	public static void writeFile(String k){
+		
+		
+	}
+	}
+
+
---
+++ Kat Zhang Projects/src/fileArray/fileArrayTest.java
@@ -0,0 +1,10 @@
+package fileArray;
+
+public class fileArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
