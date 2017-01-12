73c209fb7e47d828c78a33d7cf226a730b498753
kazhang <kazhang@192.168.109.146>
2017-01-11 22:45:36 -0500

    write/readFile() + return/enterArray(); I don't know how to test readFile()

--- Kat Zhang Projects/src/fileArray/fileArray.java
+++ Kat Zhang Projects/src/fileArray/fileArray.java
@@ -1,16 +1,64 @@
 package fileArray;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class fileArray {
 
-	public static void main(String[] args) {
 		int[] array;
+		fileArray(int[] n) {
+			array = n;
+		}
+	void writeFile(String fileName){
+        try {
+            FileWriter fw = new FileWriter(fileName);
+			String s = "";
+			for (int i = 0; i < array.length; i++){
+				s += array[i]+ "\n";
+			}
+			fw.write(s);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	fileArray(){
-	}
-	public static void writeFile(String k){
-		
-		
-	}
-	}
+      void readFile(String fileName){
+    	  FileReader fr;
+    	  try {
+  			fr = new FileReader(fileName);
+  			BufferedReader br = new BufferedReader(fr);
+  			
+  			String input;
+  			String[] dataString = null;
+  			while ((input = br.readLine()) != null) {
+  				// System.out.println(input);
+  				dataString = input.split("\n");
+  			}
+  			int[] data = new int[dataString.length];
+  			for (int i = 0; i < dataString.length; i++){
+  				array[i]=Integer.parseInt(dataString[i]);
+  			}
+  			
+  		} catch (Exception e) {
+  		}
+      }
+     int[] returnArray() {
+    			return array;
+    		}
+     String enterArray(int[]a){
+    	 String s = " ";
+			for (int i = 0; i < array.length; i++){
+				s += array[i]+ "\n";
+			}
+			return s;
+
+  	}
+
+}
+
+
 
 
--- Kat Zhang Projects/src/fileArray/fileArrayTest.java
+++ Kat Zhang Projects/src/fileArray/fileArrayTest.java
@@ -1,10 +1,26 @@
 package fileArray;
 
+import betterArray.Array;
+
 public class fileArrayTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] concertPopulation = new int[7];
+		concertPopulation[0] = 10000; // Sunday
+		concertPopulation[1] = 14120; // Monday
+		concertPopulation[2] = 10411; // Tuesday
+		concertPopulation[3] = 9000; // Wednesday
+		concertPopulation[4] = 10131; // Thursday
+		concertPopulation[5] = 9801; // Friday
+		concertPopulation[6] = 7649; // Saturday
+		
+		fileArray a = new fileArray(concertPopulation);
+		String filename = "john"; 
+		a.writeFile("john.txt");
+		a.readFile("john.txt");
+		System.out.println(a.returnArray());
+		System.out.println(a.enterArray(concertPopulation));
 	}
 
 }
