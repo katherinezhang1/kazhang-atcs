564514cb48b4c8d6b55ceff1c16b9d05ec04a290
kazhang <kazhang@192.168.109.127>
2017-02-07 12:04:22 -0500

    imported "books.csv"

--- Kat Zhang Projects/src/itemArrayList/Item.java
+++ Kat Zhang Projects/src/itemArrayList/Item.java
@@ -1,15 +1,45 @@
+
 package itemArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import betterArray.Array;
 
 public class Item {
+	
 	private int price;
 	private String name; 
 	private int quantity; 
-	
+	int[]array; 
+	Item(int[] n) {
+		array = n;
+	}
 	Item(String n, int p, int q){
 		n = name;
 		p = price; 
 		q = quantity;
 	}
+	 void readFile(String fileName){
+   	  FileReader fr;
+   	  try {
+ 			fr = new FileReader(fileName);
+ 			BufferedReader br = new BufferedReader(fr);
+ 			
+ 			String input;
+ 			String[] dataString = null;
+ 			while ((input = br.readLine()) != null) {
+ 				
+ 				System.out.println(input);
+ 				dataString = input.split("\n");
+ 			}
+ 			int[] data = new int[dataString.length];
+ 			for (int i = 0; i < dataString.length; i++){
+ 				array[i]=Integer.parseInt(dataString[i]);
+ 			}
+ 			
+ 		} catch (Exception e) {
+ 		}
+     }
 	String getName(){
 		return name;
 	}
--- Kat Zhang Projects/src/itemArrayList/itemArrayListTest.java
+++ Kat Zhang Projects/src/itemArrayList/itemArrayListTest.java
@@ -1,8 +1,12 @@
 package itemArrayList;
 
 public class itemArrayListTest {
-	
-	
+	public static void main(String[] args) {
+	int[] products = new int [0];
+	Item a = new Item (products);
+	a.readFile("books.csv");
+	System.out.println(a);
 
-	
+
+	}
 }
